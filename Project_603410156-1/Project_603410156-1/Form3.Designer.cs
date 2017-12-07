c87560c0c14cf45a5b48f2483aae81b55ada68df
sasithon2541 <33540552+sasithon2541@users.noreply.github.com>
2017-12-07 22:39:41 +0700

    form10

--- Project_603410156-1/Project_603410156-1/Form3.Designer.cs
+++ Project_603410156-1/Project_603410156-1/Form3.Designer.cs
@@ -53,7 +53,7 @@
             this.comboBox1.Font = new System.Drawing.Font("BigNoodleTitling", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.ForeColor = System.Drawing.Color.Teal;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(55, 72);
+            this.comboBox1.Location = new System.Drawing.Point(85, 72);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(311, 29);
             this.comboBox1.TabIndex = 1;
@@ -64,7 +64,7 @@
             this.button1.BackColor = System.Drawing.Color.Aqua;
             this.button1.Font = new System.Drawing.Font("BigNoodleTitling", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Navy;
-            this.button1.Location = new System.Drawing.Point(387, 68);
+            this.button1.Location = new System.Drawing.Point(417, 68);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(81, 36);
             this.button1.TabIndex = 2;
@@ -78,7 +78,7 @@
             this.button2.BackColor = System.Drawing.Color.Yellow;
             this.button2.Font = new System.Drawing.Font("BigNoodleTitling", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.Green;
-            this.button2.Location = new System.Drawing.Point(486, 68);
+            this.button2.Location = new System.Drawing.Point(516, 68);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(80, 37);
             this.button2.TabIndex = 3;
@@ -95,7 +95,7 @@
             this.tb1.Name = "tb1";
             this.tb1.ReadOnly = true;
             this.tb1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.tb1.Size = new System.Drawing.Size(511, 343);
+            this.tb1.Size = new System.Drawing.Size(575, 343);
             this.tb1.TabIndex = 4;
             // 
             // button4
@@ -104,7 +104,7 @@
             this.button4.BackColor = System.Drawing.Color.Red;
             this.button4.Font = new System.Drawing.Font("BigNoodleTitling", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.ForeColor = System.Drawing.Color.White;
-            this.button4.Location = new System.Drawing.Point(518, 470);
+            this.button4.Location = new System.Drawing.Point(590, 470);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(81, 36);
             this.button4.TabIndex = 6;
@@ -118,7 +118,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(618, 514);
+            this.ClientSize = new System.Drawing.Size(683, 514);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.tb1);
             this.Controls.Add(this.button2);
